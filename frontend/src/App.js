76a6db84a947725e1e262df625c85b5edd0455eb
computerscrib <collinscribner13@gmail.com>
2019-08-17 14:35:19 -0500

    Add react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,67 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 function App() {
+
+  var authStatus;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Route path="/login"
+               component={Login} 
+               render={(props) => <Dashboard {...props} isAuthed={authStatus}/>}
+        />
+        <Route path="/home"
+               component={Homepage} 
+               render={(props) => <Dashboard {...props} isAuthed={authStatus}/>}
+        />
+        <Route path="/user" // example URL: .../user?id=200193454
+               component={Profile} 
+               render={(props) => <Dashboard {...props} isAuthed={true}/>}
+        />
+      </div>
+    </Router>
   );
+}
+
+function Login(isAuthed) {
+
+  if (isAuthed) {
+    this.props.router.push('/home')
+  }
+
+  return (
+    <div id="login"></div>
+  );
+}
+
+function Homepage() {
+  return (
+    <div id="homepage"></div>
+  );
+}
+
+function Profile() {
+ return (
+  <div id="profile"></div>
+ );
 }
 
 export default App;
