17bd0bbb249c78de9821ef4e0b6156fe990655e5
computerscrib <collinscribner13@gmail.com>
2019-08-17 14:45:41 -0500

    Add Router changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {} from 'react-bootstrap';
 
 function App() {
 
@@ -11,23 +12,13 @@ function App() {
   return (
     <Router>
       <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/topics">Topics</Link>
-          </li>
-        </ul>
-
-        <hr />
-
         <Route path="/login"
                component={Login} 
                render={(props) => <Dashboard {...props} isAuthed={authStatus}/>}
+        />
+        <Route path="/register"
+               component={Register} 
+               render={(props) => <Dashboard {...props}/>}
         />
         <Route path="/home"
                component={Homepage} 
@@ -35,7 +26,7 @@ function App() {
         />
         <Route path="/user" // example URL: .../user?id=200193454
                component={Profile} 
-               render={(props) => <Dashboard {...props} isAuthed={true}/>}
+               render={(props) => <Dashboard {...props} isAuthed={authStatus}/>}
         />
       </div>
     </Router>
